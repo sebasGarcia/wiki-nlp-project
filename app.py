2660f8430adfa592f067fb4fa7ac1de0339d036e
Sebastián García Martínez <sebastiangarcia_m@outlook.com>
2022-08-09 14:13:08 +0200

    Adding lottie animation

--- app.py
+++ app.py
@@ -3,17 +3,32 @@ import wikipedia
 import transformers
 from transformers import pipeline, Pipeline
 import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
 
-
+#This demo is from https://www.youtube.com/watch?v=wVF0-ZalYmk&t=35s
 def main():
+    # Adding title and description
     st.title("Demo Wikipedia App")
     st.write("Search topic, ask questions, get answers")
 
+    #Only for lottie animation as a bonus..
+
+    # create a variable to load lottie animation using url
+    lottie_animation = load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_yhetm7ld.json")
+    # finally to show it
+    st_lottie(lottie_animation,
+                                height=192,
+                                width=192)
+
+    #Display topic input slot
     topic = st.text_input("SEARCH TOPIC", "")
 
     #Display article paragraph:
 
     article_paragraph = st.empty()
+
+    #Display question input slot
     question = st.text_input("QUESTION", "")
 
     if topic: 
@@ -33,6 +48,23 @@ def main():
 
             #display answer
             st.write(answer)
+
+#Lottie files for animations --> Only if we have enough time 
+# 1. pip install streamlit_lottie and then from streamlit_lottie import st_lottie
+# 2. pip install requests and import requests
+def load_lottieurl(url:str):
+    """
+    This function is used to show an animation on the webpage
+    """
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None 
+
+    return r.json()
+
+#Function for load question answering pipeline
+#returns a pipeline 
+#Definition of a pipeline --> https://huggingface.co/docs/transformers/main_classes/pipelines
 
 def load_qa_pipeline() -> Pipeline:
     qa_pipeline = pipeline("question-answering", model="distilbert-base-uncased-distilled-squad")
@@ -56,10 +88,10 @@ def load_wiki_summary(query: str ) -> str:
     summary = wikipedia.summary(results[0], sentences = 10)
     return summary
 
-    
 
 
 
+#Main app engine
 if __name__ == '__main__':
 
     main()
